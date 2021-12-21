161f8a129bd2ffb5f2533863a2a66e06388d03ed
huanglinyong <huanglinyong@mshare.cn>
2021-12-21 16:52:50 +0800

    add random

---
+++ rand/random.go
@@ -0,0 +1,27 @@
+package rand
+
+import (
+	"math/rand"
+	"time"
+)
+
+// RandomInt
+// random int
+func RandomInt(start int, end int) int {
+	rand.Seed(time.Now().UnixNano())
+	random := rand.Intn(end - start)
+	random = start + random
+	return random
+}
+
+// RandomString
+// random string
+func RandomString(len int) string {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	bytes := make([]byte, len)
+	for i := 0; i < len; i++ {
+		b := r.Intn(26) + 65
+		bytes[i] = byte(b)
+	}
+	return string(bytes)
+}
